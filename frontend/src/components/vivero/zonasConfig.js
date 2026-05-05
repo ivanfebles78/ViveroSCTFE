9a2621c027c0e6e9d4fa0ee8226b0d8aeb1d0f55
Ivan Febles <ivan.feblestrujillo@gmail.com>
2026-05-05 09:34:33 +0100

    prueba

--- frontend/src/components/vivero/zonasConfig.js
+++ frontend/src/components/vivero/zonasConfig.js
@@ -1,14 +1,14 @@
 const zonas = [
-  { id: "zona-1", apiId: "1", nombre: "Zona 1", color: "#F4E2C1", puntos: "55,1205 435,1010 415,930 580,715 930,580 910,400 795,355 320,980" },
-  { id: "zona-2", apiId: "2", nombre: "Zona 2", color: "#E87B69", puntos: "1035,665 1200,650 1555,320 1565,350 1040,680" },
-  { id: "zona-3", apiId: "3", nombre: "Zona 3", color: "#9FD486", puntos: "1050,652 1055,650 1167,500 1177,500 1027,655" },
+  { id: "zona-1", apiId: "1", nombre: "Zona 1", color: "#F4E2C1", puntos: "60,1208 440,1013 420,933 585,718 673,684 760,650 935,583 915,403 800,358 325,983" },
+  { id: "zona-2", apiId: "2", nombre: "Zona 2", color: "#E87B69", puntos: "1035,665 1046,695 1359,515 1571,370 1546,327 1298,487" },
+  { id: "zona-3", apiId: "3", nombre: "Zona 3", color: "#9FD486", puntos: "1028,667 1171,573 1160,530 1076,589 1000,637" },
   { id: "zona-4", apiId: "4", nombre: "Zona 4", color: "#BFD9EA", puntos: "1195,330 1295,315 1295,455 1190,450" },
-  { id: "zona-4a", apiId: "4a", nombre: "Zona 4 A", color: "#F5D547", puntos: "1380,750 1555,700 1650,720 1650,800 1465,825 1370,810" },
-  { id: "zona-4b", apiId: "4b", nombre: "Zona 4 B", color: "#F08A80", puntos: "1455,800 1655,725 1650,880 1460,925 1385,870" },
+  { id: "zona-4a", apiId: "4a", nombre: "Zona 4 A", color: "#F5D547", puntos: "1366,754 1510,711 1530,741 1623,709 1650,770 1431,831 1336,798" },
+  { id: "zona-4b", apiId: "4b", nombre: "Zona 4 B", color: "#F08A80", puntos: "1430,839 1655,774 1672,867 1486,919 1414,875" },
   { id: "zona-5", apiId: "5", nombre: "Zona 5", color: "#F3CF39", puntos: "920,790 435,1030 425,975 890,740" },
   { id: "zona-6", apiId: "6", nombre: "Zona 6", color: "#A7D98C", puntos: "740,1055 705,975 860,915 865,1000" },
-  { id: "zona-7", apiId: "7", nombre: "Zona 7", color: "#9ECBE2", puntos: "685,1086 865,1215 430,1095 215,1080 82,1094 501,885 555,997 663,943 649,873 677,858 693,867 646,1061" },
-  { id: "zona-8", apiId: "8", nombre: "Zona 8", color: "#F3E0BD", puntos: "1340,710 1075,1250 1255,1300" },
+  { id: "zona-7", apiId: "7", nombre: "Zona 7", color: "#9ECBE2", puntos: "871,1163 924,1183 896,1278 584,1183 284,1174 107,1194 678,914 730,1057 816,1029 887,987 868,910 919,878 943,891" },
+  { id: "zona-8", apiId: "8", nombre: "Zona 8", color: "#F3E0BD", puntos: "1491,661 1335,749 1238,842 1107,1095 1075,1250 1218,1306 1600,1271 1716,1021 1663,1034 1678,874 1452,937 1331,802 1351,754 1524,707 1538,665" },
   { id: "zona-9", apiId: "9", nombre: "Zona 9", color: "#F7E85B", puntos: "1170,570 1545,320 1520,250 1205,430" },
   { id: "zona-10a", apiId: "10a", nombre: "Zona 10 A", color: "#6BAED6", puntos: "690,970 840,915 825,825 675,915" },
   { id: "zona-10b", apiId: "10b", nombre: "Zona 10 B", color: "#4E8BC5", puntos: "935,885 1000,915 920,1190 860,1170" },
@@ -16,4 +16,3 @@ const zonas = [
 ];
 
 export default zonas;
-
